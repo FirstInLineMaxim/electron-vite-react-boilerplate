6407c4dbba5f2f36530738d93ec406b50033ac5e
firstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-07-01 15:36:11 +0200

    init git

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import electron from 'vite-plugin-electron'
+import renderer from 'vite-plugin-electron-renderer'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(), 
+      electron([{
+        entry: 'electron/main.ts',
+      },{
+        entry: "electron/preload.ts",
+        onstart(options) {
+          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
+          // instead of restarting the entire Electron App.
+          options.reload();
+        },
+      },]),
+    renderer()
+    ],
+})
